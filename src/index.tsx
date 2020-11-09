7fe3a81c0e18996af83dab3bdd5651ab3764b63b
U <U@yutaronoMacBook-Pro.local>
2020-11-09 10:59:29 +0900

    add reportwebvitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
 
 ReactDOM.render(
@@ -14,4 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
+reportWebVitals();
